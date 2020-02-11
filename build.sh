e8c82c6f821b0432f98d8e8b9d360f28d704d62e
Hatsune-Miku <49642240+ThinkerJ@users.noreply.github.com>
2020-02-11 15:58:30 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,25 @@
 echo "Initialize build system...."
-echo "[1/7] Cleaning Directory....."
+echo "[1/9] Cleaning Directory....."
 rm -rf ~/tool
 rm -rf ~/$(pwd)/output
 rm -rf ~/$(pwd)/zip
-echo "[2/7] Cloning ToolChain....."
+echo "[2/9] Cloning ToolChain....."
 git clone https://github.com/najahiiii/aarch64-linux-gnu.git -b linaro8-20190216 ~/tool
-echo "[3/7] Toolchain has been Cloned......"
+echo "[3/9] Toolchain has been Cloned......"
 export ARCH=arm64
 export CROSS_COMPILE=~/tool/bin/aarch64-linux-gnu-
-echo "[4/7] Mkdir output......"
+echo "[4/9] Mkdir output......"
 mkdir output
-echo "[5/7] Make Kernel......"
+echo "[5/9] Make Kernel......"
 make -C $(pwd) O=output ysl_defconfig
 make -j32 -C $(pwd) O=output
-echo "[6/7] Copy Image.dtb....."
+echo "[6/9] Cloning some stuff...."
 git clone https://github.com/mahajant99/AnyKernel3.git -b ysl-aosp zip
+echo "[7/9] Stuff cloned..."
 cp -r output/arch/arm64/boot/Image.gz-dtb zip/
 cd zip
+echo "[8/9] copy = image.dtb"
+echo "Create Flashable zip"
 mv Image.gz-dtb zImage 
 zip -r Miku-Kernel-Test.zip *
 echo "[7/7] Finished...."
