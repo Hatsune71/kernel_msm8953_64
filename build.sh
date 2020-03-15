383f471fc5e6b094b0b7f427f62d2f3eb32e4dcb
Hatsune-Miku <49642240+ThinkerJ@users.noreply.github.com>
2020-03-16 00:34:55 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,23 +29,3 @@ echo "Create Flashable zip"
 mv Image.gz-dtb zImage 
 zip -r Miku-Kernel-Test.zip *
 echo "[9/9] Finished...."
- ~/tool
-echo "[3/9] Toolchain has been Cloned......"
-export ARCH=arm64
-export CROSS_COMPILE=~/tool/bin/aarch64-linux-gnu-
-echo "[4/9] Mkdir output......"
-mkdir output
-echo "[5/9] Make Kernel......"
-make -C $(pwd) O=output ysl_defconfig
-make -j32 -C $(pwd) O=output
-echo "[6/9] Cloning some stuff...."
-git clone https://github.com/mahajant99/AnyKernel3.git -b ysl-aosp zip
-echo "[7/9] Stuff cloned..."
-cp -r output/arch/arm64/boot/Image.gz-dtb zip/
-cd zip
-echo "[8/9] copy = image.dtb"
-echo "Create Flashable zip"
-mv Image.gz-dtb zImage 
-zip -r Miku-Kernel-Test.zip *
-echo "[9/9] Finished...."
-
